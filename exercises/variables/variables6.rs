c52ee76b85611ffd207abd32a99786d33bf7db5b
YunShu <im@yunshu.site>
2024-04-03 23:13:59 +0800

    feat: finish variables

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -3,9 +3,8 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+// 不是吧，rust的常量竟然不能自己推导？
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
