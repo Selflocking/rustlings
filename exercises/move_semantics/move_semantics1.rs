12f9c45b276e4fb991bd32ab976ccd443098d627
YunShu <im@yunshu.site>
2024-04-04 15:04:32 +0800

    feat: finish move_semantics

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
@@ -14,8 +12,13 @@ fn main() {
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
+// move 进来, 原本的vec是immut的，加上mut标识为可以更改的
+// fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+// 等效于下面两行
+// fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec;
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
